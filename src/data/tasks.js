804c7b3ee543dcbacb870bb24f24ef51eb4a67bb
carolacodes <carolacardozog23@gmail.com>
2024-12-13 20:23:54 -0300

    firt commit

---
+++ src/data/tasks.js
@@ -0,0 +1,24 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'mi primer tarea',
+        description: 'mi primer tarea'
+    },
+    {
+        id: 1,
+        title: 'mi segunda tarea',
+        description: 'mi segunda tarea'
+    },
+    {
+        id: 2,
+        title: 'mi tercer tarea',
+        description: 'mi tercer tarea'
+    },
+    {
+        id: 4,
+        title: 'mi cuarta tarea',
+        description: 'mi cuarta tarea'
+    },
+
+]
+
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import { TaskContextProvider } from './context/TaskContext'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <TaskContextProvider>
+      <App />
+    </TaskContextProvider>
+  </StrictMode>,
+)
